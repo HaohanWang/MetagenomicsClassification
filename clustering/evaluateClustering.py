93d0b58df1d4c1644ae76f56a1d97734cebcd8e0
HaohanWang <haohanw@andrew.cmu.edu>
2018-02-22 20:28:41 -0500

    code update

---
+++ clustering/evaluateClustering.py
@@ -0,0 +1,28 @@
+__author__ = 'Haohan Wang'
+
+from sklearn.metrics import adjusted_rand_score as ars
+
+def matchClusteringLabels(filename):
+    text = [line.strip() for line in open('/home/haohanw/metagenomics/clusteringResult_'+filename+'.txt')]
+
+    result = {}
+    for line in text:
+        items = line.split('\t')
+        result[items[0]] = int(items[1])
+
+    rs1 = []
+    rs2 = []
+    text = [line.strip() for line in open('/home/haohanw/metagenomics/Kaiju/'+filename+'.label')]
+    for line in text:
+        items = line.split('\t')
+        rs1.append(int(items[1]))
+        rs2.append(result[[items[0]]])
+
+    print 'organizing results, now calculating'
+
+    a = ars(rs1, rs2)
+    print 'final score', abs(a)
+
+if __name__ == '__main__':
+    filename = ''
+    matchClusteringLabels(filename)
