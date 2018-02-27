a8bd543e591b4e8bfce45cf474efb9000b8bb135
HaohanWang <haohanw@andrew.cmu.edu>
2018-02-27 18:16:00 -0500

    code update

--- scripts/crossValidation.sh
+++ scripts/crossValidation.sh
@@ -8,7 +8,7 @@
 #SBATCH --mem=80000
 
 # set max wallclock time
-#SBATCH --time=8:00:00
+#SBATCH --time=16:00:00
 
 # set name of job
 #SBATCH --job-name=mellon
@@ -24,9 +24,9 @@
 # /home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high original nb
 # /home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high ico nb
 # /home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high kmer nb
-# /home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high original svc
-# /home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high ico svc
-# /home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high kmer svc
-/home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high original lr
-/home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high ico lr
-/home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high kmer lr
+/home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high original svc
+/home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high ico svc
+/home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high kmer svc
+# /home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high original lr
+# /home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high ico lr
+# /home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high kmer lr
