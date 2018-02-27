8ec0e5065064f8b1b6045405d5e6b9ff026cae8f
HaohanWang <haohanw@andrew.cmu.edu>
2018-02-27 17:04:07 -0500

    code update

--- scripts/crossValidation.sh
+++ scripts/crossValidation.sh
@@ -24,9 +24,9 @@
 /home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high original nb
 /home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high ico nb
 /home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high kmer nb
-/home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high original svc
-/home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high ico svc
-/home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high kmer svc
-/home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high original lr
-/home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high ico lr
-/home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high kmer lr
+# /home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high original svc
+# /home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high ico svc
+# /home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high kmer svc
+# /home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high original lr
+# /home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high ico lr
+# /home/haohanw/python2.7/bin/python /home/haohanw/metaGenomicsClustering/classification/crossValidation.py mellon_high kmer lr
